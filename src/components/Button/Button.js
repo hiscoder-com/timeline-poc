a3e25c93f9101392bc27ad17d5697e07338d05d9
Vitek Foxprogs <foxprogs@gmail.com>
2022-02-23 18:17:59 +0400

    wip 1st version

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,30 +4,10 @@ import { Timeline } from '@knight-lab/timelinejs';
 // import '@knight-lab/timelinejs/dist/css/timeline.css';
 import { log } from '../../utils';
 import PropTypes from 'prop-types';
-
-const events = [
-  {
-    start_date: {
-      year: '-2000',
-    },
-    end_date: {
-      year: '-1700',
-    },
-    text: {
-      headline: 'A headline',
-      text: 'Some text here',
-    },
-  },
-  {
-    start_date: {
-      year: '-1300',
-    },
-    text: {
-      headline: 'Another headline',
-      text: '<h2>Some more text here</h2> Hello <b>WORLD</b>',
-    },
-  },
-];
+import axios from 'axios';
+import ReactMarkdown from 'react-markdown';
+import { useRemark } from 'react-remark';
+import showdown from 'showdown';
 
 const options = {
   // height: '1000px',
@@ -40,9 +20,49 @@ const options = {
 
 function Button({ text, onClick }) {
   log('test');
+  const [events, setEvents] = React.useState([]);
+  const [reactContent, setMarkdownSource] = useRemark();
 
   const timelineEl = React.useRef(null);
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+    axios
+      .get('https://git.door43.org/BSA/ru_timeline/raw/branch/master/kings.tsv')
+      .then((axiosRes) => {
+        const data = axiosRes.data;
+        const dataList = data
+          .split('\n')
+          .map((el) => el.split('\t'))
+          .slice(1);
+        const _events = [];
+        dataList.forEach((element) => {
+          const [startYear, startMonth, startDay] = element[1].split('.');
+          let start_date;
+          if (startYear) {
+            start_date = {
+              year: startYear,
+              month: startMonth ?? '1',
+              day: startDay ?? '1',
+            };
+          }
+          const [endYear, endMonth, endDay] = element[2].split('.');
+          const converter = new showdown.Converter();
+          _events.push({
+            start_date,
+            end_date: {
+              year: endYear,
+              month: endMonth ?? '1',
+              day: endDay ?? '1',
+            },
+            text: {
+              headline: element[3],
+              text: converter.makeHtml(element[4].replaceAll('\\n', '\n')),
+            },
+          });
+        });
+        setEvents(_events);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   React.useEffect(() => {
     if (timelineEl.current) {
